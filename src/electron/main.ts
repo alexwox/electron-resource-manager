d5914dbf90ec2529d85da7fc45d5158efca711b4
alexwox <alexander.woxstrom@gmail.com>
2025-05-29 14:09:32 +0200

    Read cpu, ram, storage

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
+import { pollResources } from "./resourceManager.js";
 
 type test = string;
 
@@ -14,4 +15,6 @@ app.on("ready", () => {
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
   }
+
+  pollResources();
 });
---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,33 @@
+import osUtils from "os-utils";
+import fs from "fs";
+const POLLING_INTERVAL = 500;
+
+export function pollResources() {
+  setInterval(async () => {
+    const cpuUsage = await getCpuUsage();
+    const ramUsage = await getRamUsage();
+    const storageUsage = await getStorageData();
+    console.log({ cpuUsage, ramUsage, storageUsage: storageUsage.usage });
+  }, POLLING_INTERVAL);
+}
+
+function getCpuUsage() {
+  return new Promise((resolve) => {
+    osUtils.cpuUsage(resolve);
+  });
+}
+
+function getRamUsage() {
+  return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/");
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+
+  return {
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
+}
